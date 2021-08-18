efd55f5cb975c38b408f2aa570c0eb1601cda1e3
huaziran <1215756459@qq.com>
2021-08-18 18:13:22 +0800

    算法

---
+++ Algorithm/src/com/huaziran/floyd/FloyedAlgorithm.java
@@ -0,0 +1,83 @@
+package com.huaziran.floyd;
+
+import java.util.Arrays;
+
+public class FloyedAlgorithm {
+    public static void main(String[] args) {
+        //测试图
+        char[] vertex = {'A', 'B', 'C', 'D', 'E', 'F', 'G'};
+        //创建临界矩阵
+        int[][] matrix = new int[vertex.length][vertex.length];
+        final int N = 65535;
+        matrix[0] = new int[]{0, 5, 7, N, N, N, 2};
+        matrix[1] = new int[]{5, 0, N, 9, N, N, 3};
+        matrix[2] = new int[]{7, N, 0, N, 8, N, N};
+        matrix[3] = new int[]{N, 9, N, 0, N, 4, N};
+        matrix[4] = new int[]{N, N, 8, N, 0, 5, 4};
+        matrix[5] = new int[]{N, N, N, 4, 5, 0, 6};
+        matrix[6] = new int[]{2, 3, N, N, 4, 6, 0};
+        Graph graph = new Graph(vertex.length, matrix, vertex);
+        graph.floyd();
+        graph.show();
+    }
+}
+
+class Graph {
+    private char[] vertex;
+    private int[][] dis;//存放距离
+    private int[][] pre; //存放前驱节点
+
+    /**
+     * @param length 大小
+     * @param matrix 临界矩阵
+     * @param vertex 顶点数组
+     */
+    public Graph(int length, int[][] matrix, char[] vertex) {
+        this.vertex = vertex;
+        this.dis = matrix;
+        this.pre = new int[length][length];
+        //对pre数组初始化，注意存放的是前驱顶点的下标
+        for (int i = 0; i < length; i++) {
+            Arrays.fill(pre[i], i);
+        }
+    }
+
+    //显示pre数组和dis数组
+    public void show() {
+        char[] vertex = {'A', 'B', 'C', 'D', 'E', 'F', 'G'};
+
+        for (int k = 0; k < dis.length; k++) {
+            //
+            for (int i = 0; i < dis.length; i++) {
+                System.out.print(vertex[pre[k][i]] + " ");
+//                System.out.print(pre[k][i] + " ");
+            }
+            System.out.println();
+            for (int i = 0; i < dis.length; i++) {
+                System.out.print("(" + vertex[k] + "->" + vertex[i] + " min:" + dis[k][i] + ")");
+            }
+            System.out.println();
+        }
+
+    }
+
+    //佛洛依德算法
+    public void floyd() {
+        int len = 0;//变量保存距离
+        //对中间顶点遍历，k就是中间顶点下标
+        for (int k = 0; k < dis.length; k++) {
+            //从i出发
+            for (int i = 0; i < dis[k].length; i++) {
+                //到达j
+                for (int j = 0; j < dis.length; j++) {
+                    len = dis[i][k] + dis[k][j]; //从i出发，通过k，到达j
+                    if (len < dis[i][j]) {
+                        //如果len<dis[i][j]
+                        dis[i][j] = len;//更新距离
+                        pre[i][j] = pre[k][j];//更新 前驱顶点
+                    }
+                }
+            }
+        }
+    }
+}
